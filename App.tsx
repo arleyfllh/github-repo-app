4cd36bbc9e34a1fdab85d9e0edbc7db2b16b9b5c
Arley Fallah <arleyfllh@gmail.com>
2025-05-21 16:43:02 +0700

    Add dependencies and adjust App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import store from "./src/redux/store";
+import { Provider } from "react-redux";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import UserProfileScreen from "./src/screens/UserProfileScreen";
+import SelfProfileScreen from "./src/screens/SelfProfileScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={HomeScreen}/>
+          <Stack.Screen name='UserProfile' component={UserProfileScreen}/>
+          <Stack.Screen name='SelfProfile' component={SelfProfileScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
